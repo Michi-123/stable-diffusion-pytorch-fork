c1f485436da85c1ad80b0aafe2b0e2638d6f7965
Michiyasu Uchiyama <hairstyle2004@yahoo.co.jp>
2023-08-11 01:44:13 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(current_path, 'README.md'), encoding='utf-8') as f:
 setup(
     name='stable-diffusion-pytorch', 
     packages=find_packages(exclude=('*.pyc',)),
-    version='0.0.6',
+    version='0.1.0',
     license='MIT', 
     install_requires=[],
     author='Michiyasu Uchiyama',
